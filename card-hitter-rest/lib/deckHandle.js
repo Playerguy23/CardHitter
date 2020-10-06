b757830b734f83db5faaf72c41eddef80a122e49
Playerguy23 <26669547+Playerguy23@users.noreply.github.com>
2020-10-06 12:40:17 +0300

    Fixed randomization.

--- card-hitter-rest/lib/deckHandle.js
+++ card-hitter-rest/lib/deckHandle.js
@@ -23,7 +23,7 @@ const changeSwapValuesInArray = (deck) => {
 
 const suffleDeck = (deck) => {
     let returnDeck = deck;
-    let randomNum = Math.round(Math.random(90 - 50) + 50);
+    let randomNum = Math.round(Math.random(90 - 70) + 70);
 
     for (let i = 0; i < randomNum; i++) {
         returnDeck = changeSwapValuesInArray(returnDeck);
